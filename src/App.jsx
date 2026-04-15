c725a89bae428924432d12fa0e1553be9bf1dbea
Breno Orem <breno.orem@gmail.com>
2026-04-15 16:16:06 -0300

    Finalização do Site

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import Header from "./components/Header"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import NothingHere from "./pages/NothingHere";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-slate-900">
-      <div className="flex justify-center border-top-4">
-        <Header />
-      </div>
-      <h1 className="flex justify-center items-center h-screen">Tarefa 06</h1>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="w-screen h-screen bg-[#fefae0]">
+              <div className="flex justify-center border-top-4">
+                <Header />
+              </div>
+
+              <div className="flex h-full w-full items-center justify-center">
+                <div className="w-3/5 h-4/6 bg-[#283618] rounded-[1.2vw]">
+                  <div className="flex flex-col h-full justify-center items-center font-serif">
+                    <div className=" text-[#fefae0] text-9xl"><h1>Desennvolvimento Web</h1></div>
+                    <div className="text-[#e2dec1] text-8xl"><h2>Tarefa 06</h2></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
+        />
+        <Route path="/NothingHere" element={<NothingHere />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
+import { Link, useNavigate } from "react-router";
+
 function Header() {
-    return (
-        <div className="text-white font-bold bg-blue-950 w-9/12 p-4 rounded-[1.2vw] flex flex-row justify-between">
-            <a href="../pages/ToDoList">To-Do List</a>
-            <a href="../pages/ClickCounter">Contador de Cliques</a>
-            <a href="../pages/TicTacToe">Jogo da Velha</a>
-            <a href="../pages/Calculator">Calculadora</a>
-            <a href="../pages/CepFinder">Buscador de CEP</a>
+  const navigate = useNavigate();
+
+  function onClickHeader() {
+    navigate("/NothingHere");
+  }
+
+  return (
+    <div className="relative flex w-full justify-center">
+      <div className="flex justify-center bg-[#606c38] w-98/100 p-8 rounded-[0.4vw] absolute top-4">
+        <div className="text-white font-serif text-xl flex w-1/2 justify-evenly">
+          <button onClick={() => navigate(`/`)}>Home</button>
+          <button onClick={onClickHeader}>To-Do List</button>
+          <button onClick={onClickHeader}>Contador de Cliques</button>
+          <button onClick={onClickHeader}>Jogo da Velha</button>
+          <button onClick={onClickHeader}>Calculadora</button>
+          <button onClick={onClickHeader}>Buscador de CEP</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
