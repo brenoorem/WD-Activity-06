bc35e702969b343724dc5ca85e7cea1a380e5b5b
Breno Orem <breno.orem@gmail.com>
2026-04-15 16:21:47 -0300

    Ajuste de Estilo

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ function App() {
               <div className="flex h-full w-full items-center justify-center">
                 <div className="w-3/5 h-4/6 bg-[#283618] rounded-[1.2vw]">
                   <div className="flex flex-col h-full justify-center items-center font-serif">
-                    <div className=" text-[#fefae0] text-9xl"><h1>Desennvolvimento Web</h1></div>
-                    <div className="text-[#e2dec1] text-8xl"><h2>Tarefa 06</h2></div>
+                    <div className=" text-[#fefae0] text-8xl"><h1>Desennvolvimento Web</h1></div>
+                    <div className="text-[#e2dec1] text-7xl"><h2>Tarefa 06</h2></div>
                   </div>
                 </div>
               </div>
